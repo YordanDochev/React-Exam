82919364c26525b88216118d84485636b170a675
YordanDochev <yordan.dochev13@gmail.com>
2023-11-29 01:12:59 +0200

    Render home page estates

--- client/src/components/homePageComponent/HotOffers.jsx
+++ client/src/components/homePageComponent/HotOffers.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import * as estateService from '../../services/estateService'
+import EstateItem from "./EstatesItem";
 
 export default function HotOffers(){
     const [estates,setEstates] = useState([])
@@ -36,27 +37,9 @@ export default function HotOffers(){
                 <div className="tab-content">
                     <div id="tab-1" className="tab-pane fade show p-0 active">
                         <div className="row g-4">
-                            <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                                <div className="property-item rounded overflow-hidden">
-                                    <div className="position-relative overflow-hidden">
-                                        <Link to="/details"><img className="img-fluid" src="src/assets/img/property-1.jpg" alt=""/></Link>
-                                        <div className="bg-primary rounded text-white position-absolute start-0 top-0 m-4 py-1 px-3">For Sell</div>
-                                        <div className="bg-white rounded-top text-primary position-absolute start-0 bottom-0 mx-4 pt-1 px-3">Appartment</div>
-                                    </div>
-                                    <div className="p-4 pb-0">
-                                        <h5 className="text-primary mb-3">$12,345</h5>
-                                        <a className="d-block h5 mb-2" href="">Golden Urban House For Sell</a>
-                                        <p><i className="fa fa-map-marker-alt text-primary me-2"></i>123 Street, New York, USA</p>
-                                    </div>
-                                    <div className="d-flex border-top">
-                                        <small className="flex-fill text-center border-end py-2"><i className="fa fa-ruler-combined text-primary me-2"></i>1000 Sqft</small>
-                                        <small className="flex-fill text-center border-end py-2"><i className="fa fa-bed text-primary me-2"></i>3 Bed</small>
-                                        <small className="flex-fill text-center py-2"><i className="fa fa-bath text-primary me-2"></i>2 Bath</small>
-                                    </div>
-                                </div>
-                            </div>
+                            {estates.map(estate => <EstateItem key={estate._id} estate={estate} />)}
                         
-                            <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                            {/* <div className="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                                 <div className="property-item rounded overflow-hidden">
                                     <div className="position-relative overflow-hidden">
                                         <a href=""><img className="img-fluid" src="src/assets/img/property-2.jpg" alt=""/></a>
@@ -93,7 +76,7 @@ export default function HotOffers(){
                                         <small className="flex-fill text-center py-2"><i className="fa fa-bath text-primary me-2"></i>2 Bath</small>
                                     </div>
                                 </div>
-                            </div>
+                            </div> */}
 
                             <div className="col-12 text-center wow fadeInUp" data-wow-delay="0.1s">
                                 <Link to="/catalog" className="btn btn-primary py-3 px-5" href="">Browse More Property</Link>
